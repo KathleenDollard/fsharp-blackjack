221479698067d5ca02a0e7a4532f5a14d8c016a7
Kathleen Dollard <kdollard@microsoft.com>
2022-12-03 07:32:25 -0800

    Split game from program and fixes

---
+++ src/fsharp-blackjack/Blackjack.fs
@@ -0,0 +1,115 @@
+﻿
+module Blackjack
+    open CardDeck
+
+    type Player =
+        { Name: string
+          Hand: Card list
+          Strategy: Strategy
+          LastAction: Action
+          IsDealer : bool }
+
+    and Action =
+        | None
+        | Hit
+        | Stand
+
+    and Strategy = Player -> Action
+
+    // TODO: Consider DU for bust, blackjack, and score
+    let score ({ Hand = cards }) = 
+
+        let cardScore aceOne card =
+            match card with
+            | FaceCard (_, _) -> 10
+            | ValueCard (_, value) -> value
+            | Ace (_) -> if aceOne then 1 else 11
+            
+        let cardScoreAceLow = cardScore true
+        let cardScoreAceHigh = cardScore false
+        let lowScore = cards |> List.sumBy cardScoreAceLow
+        let highScore =   cards |> List.sumBy cardScoreAceHigh
+        let score = if highScore > 21 then lowScore else highScore
+        if score > 21 then 0 else score
+      
+    let hit deck player =
+        let cards, deck  = draw 1 deck
+        let player = { player with Hand = player.Hand |> List.append cards }
+        deck, player
+
+    let aggressivePlayer: Strategy = fun player -> Hit
+
+    let defensivePlayer: Strategy = fun player -> Stand
+
+    let standAt (target: int) (player: Player) =
+        if score player >= target then
+            Stand
+        else
+            Hit
+
+    // The dealer strategy is set by table rules
+    let dealerStrategy: Strategy =
+        standAt 17
+
+    let gameOver players =
+        players |> List.exists (fun player -> player.LastAction <> Stand )
+
+    let playerTurn deck player  =
+        // @Chet: I find the following line a bit odd. Seems a smell
+        let action = player.Strategy player
+        match action with 
+        | Hit -> hit deck player
+        | None
+        | Stand -> (deck, player)
+
+    let rec turns deck players =
+        if gameOver players then 
+            deck, players
+        else
+            let mutable deck = deck
+            let newPlayers = [
+                for player in players do
+                    let newDeck, player = playerTurn deck player
+                    deck <- newDeck
+                    player
+            ]
+            turns deck newPlayers
+
+    let gameSetup players =
+        let deck = fullDeck () |> shuffle
+        let deck, hands = deal 2 deck (List.length players)
+        let zipped = players |> List.zip hands
+        let players = zipped |> List.map (fun(hand, player) -> { player with Hand = hand})
+        deck, players
+
+    let reportOutcome dealerScore winners losers =
+        if List.length winners = 0 then 
+             printfn $"Dealer wins with a score of {dealerScore}"
+        else
+             printfn $"Dealer score is {dealerScore}"
+             for player in winners do
+                 printfn $"Player {player.Name} wins with a score of {score player}"
+
+        for player in losers do
+             printfn $"Player {player.Name} loses with a score of {score player}"
+
+
+    let dealer =
+        { Name = "Dealer"
+          Hand = []
+          LastAction = None
+          IsDealer = true
+          Strategy = dealerStrategy }
+
+    let playGame players =
+        let players = players |> List.insertAt 0 dealer
+        let deck, players = gameSetup players
+        let _, players = turns deck players
+
+        let dealerScore = score dealer
+        let nonDealers = players |> List.filter (fun player -> not player.IsDealer)
+        let winners, losers = nonDealers |> List.partition (fun player -> score player > dealerScore)
+        
+        reportOutcome dealerScore winners losers
+ 
+        None
