9a6fb243d9f6dc601376dfa554e9171f998f40da
Kathleen Dollard <kdollard@microsoft.com>
2022-12-05 08:05:32 -0800

    Added a more readable approach to splitting up the indexed cards

--- src/fsharp-card-deck/CardDeck.fs
+++ src/fsharp-card-deck/CardDeck.fs
@@ -49,17 +49,36 @@ let draw numberOfCards (deck: Deck) =
 let drawOne = draw 1
 
 let shuffle inDeck : Deck =
-    inDeck
-    |> List.sortBy (fun c -> Random.Shared.Next())
+    inDeck |> List.sortBy (fun c -> Random.Shared.Next())
 
-// Dealing implies each player gets one card
 let deal numberOfCards numberOfPlayers deck =
+    let removeIndexFromCards cards =
+        cards |> List.map (fun (_, card) -> card)
+
     let countCardsToDeal = numberOfCards * numberOfPlayers
 
+    // Split deck to cards to deal and remaining cards
     if (List.length deck) < countCardsToDeal then
         failwith "Not enough cards in deck"
     let cardsToDeal, remainingCards = deck |> List.splitAt countCardsToDeal
 
+    // Add an index, group on modulus to deal, remove indexes (see picture in slid deck)
+    let indexed = cardsToDeal |> List.indexed // creates a tupled list with index/member
+    let (_, groups) = indexed |> List.groupBy (fun (pos, _) -> pos % numberOfPlayers)
+                              |> List.unzip
+    let hands = groups |> List.map removeIndexFromCards
+
+    hands, remainingCards
+
+let dealOld numberOfCards numberOfPlayers deck =
+    let countCardsToDeal = numberOfCards * numberOfPlayers
+
+    // Split deck to cards to deal and remaining cards
+    if (List.length deck) < countCardsToDeal then
+        failwith "Not enough cards in deck"
+    let cardsToDeal, remainingCards = deck |> List.splitAt countCardsToDeal
+
+    // Add an index, group, remove indexes
     let indexed = cardsToDeal |> List.indexed // creates a tupled list with index/member
     let groups = indexed |> List.groupBy (fun (pos, _) -> pos % numberOfPlayers)
     let hands = 
